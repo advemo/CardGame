91e5a61ca790629dad25f41fb14f350c2c04092a
Adrian <adrian.vento87@gmail.com>
2020-07-09 19:22:04 -0700

    Upload Images

--- script.js
+++ script.js
@@ -5,17 +5,30 @@ var tier1 = document.getElementById('Tier1');
 var tier2 = document.getElementById('Tier2');
 var tier3 = document.getElementById('Tier3');
 
+// var pic1 = document.getElementById('pic1');
+// var pic2 = document.getElementById('pic2');
+// var pic3 = document.getElementById('pic3');
+// var pic4 = document.getElementById('pic4');
+// var pic5 = document.getElementById('pic5');
+// var pic6 = document.getElementById('pic6');
 
-//show cards
+var Img1Count;
+var Img2Count;
+var LastImageFlipped;
+var CurentImageFlipped;
+
+//PLAY AND RESET BUTTON 
 buttonPlay.addEventListener('click', showCards);
 buttonReset.addEventListener('click', hideCards);
 
+
+// DISPLAY CARDS AFTER SHOSSING LEVEL
+
 function showCards(e){
         e.preventDefault();
-    
+  
   if (document.getElementById('select-game').value === '1') {
     tier1.style.visibility = "visible";
-
         }
 
     if (document.getElementById('select-game').value === '2') {
@@ -33,27 +46,71 @@ document.getElementById('select-game').value = "0";
 
 }
 
+// RESET GAME
 function hideCards(e){
    e.preventDefault();
 
    tier1.style.visibility = "hidden";
-   tier2.style.visibility = "hidden";
-   tier3.style.visibility = "hidden";
+  //  tier2.style.visibility = "hidden";
+  //  tier3.style.visibility = "hidden";
 
 }
 
-var imgClicked = document.getElementById('box-1');
-var imgClicked2 = document.getElementById('box-2');
 
-imgClicked.addEventListener('click', imageClicked);
 
-  function imageClicked()
-  {
-    document.getElementById('pic1').visibility = 'visible';
+function CompareImages(img1, img2, SetThisOne, Current) {
+   if (img1 =! img2) {
+     CurentImageFlipped = SetThisOne;
+     Current = SetThisOne;
 
-    console.log(document.getElementById('pic1'));
-    
+   } 
+
+   
+   console.log(img1);
+   console.log(img2);
 }
+
+
+function toggleImage(ImgName, NewImg) {
+   
+  var img11 = "img/Init.jpg";
+   var imgElement = document.getElementById(ImgName);
+   imgElement.src =  NewImg;
+
+   CurentImageFlipped = imgElement;
+
+
+   CompareImages(LastImageFlipped, CurentImageFlipped, img11, imgElement);
+
+   LastImageFlipped = NewImg;
+
+console.log(imgElement);
+console.log(CurentImageFlipped);
+console.log(Img1Count);
+console.log(Img2Count);
+console.log(LastImageFlipped);
+console.log();
+
+CurentImageFlipped = "";
+
+}
+
+
+
+
+
+
+
+
+// function ShowClickedImage(ImgName, Source){
+
+//   image = document.getElementById(ImgName);
+//   image.src = Source;
+
+// }
+
+
+
 
 // imgClicked2.addEventListener('click', imageClicked2);
 
@@ -67,79 +124,3 @@ imgClicked.addEventListener('click', imageClicked);
 
 
 
-
-
-// var form = document.getElementById('addForm');
-// var itemList = document.getElementById('items');
-// var filter = document.getElementById('filter');
-// // form submit event
-// form.addEventListener('submit' , addItem);
-
-// //delete event
-// itemList.addEventListener('click', removeItem);
-
-// //filter items
-// filter.addEventListener('keyup', filterItems);
-
-
-// function addItem(e){
-//    e.preventDefault();
-
-//    //get input value
-// if (document.getElementById('item').value != '') {
-    
-
-//    var newItem = document.getElementById('item').value;
-
-//    // crete new li element
-   
-//    var li = document.createElement('li');
-//    //add class
-//    li.className = 'list-group-item';
-//    //add text node with input value
-//    li.appendChild(document.createTextNode(newItem));
- 
-//    // create de delete button element
-//    var deleteBtn = document.createElement('button');
-//    // add clases to dlete button
-//    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-//    // append text node
-//    deleteBtn.appendChild(document.createTextNode('X'));
-
-//    //append button to li
-//    li.appendChild(deleteBtn);
-
-//    //append li to list
-//    itemList.appendChild(li);
-   
-//   }
-//   document.getElementById('item').value = '';
-// }
-// //remove item
-
-// function removeItem(e){
-//    if (e.target.classList.contains('delete')) {
-//        if (confirm('Are you sure?')) {
-//          var li = e.target.parentElement; 
-//          itemList.removeChild(li);
-//        }
-
-//    }
-
-// }
-
-// function filterItems(e){
-//    // convert to lowercase
-//    var text = e.target.value.toLowerCase();
-//     var items = itemList.getElementsByTagName('li');
-//    // convert to array
-//    Array.from(items).forEach(function(item){
-//        var itemName = item.firstChild.textContent;
-//        if (itemName.toLowerCase().indexOf(text) != -1) {
-//            item.style.display = 'block';
-//         }  else{
-//              item.style.display = 'none';
-//            }
-//       });
-// }
