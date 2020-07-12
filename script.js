71df9096c98c0fb43deb5d0b646e5a1abaf3b309
Adrian <adrian.vento87@gmail.com>
2020-07-11 18:36:58 -0700

    Images compares and match

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
-var buttonPlay = document.getElementById('play');
-var buttonReset = document.getElementById('reset');
-var tier1 = document.getElementById('Tier1');
-var tier2 = document.getElementById('Tier2');
-var tier3 = document.getElementById('Tier3');
+var buttonPlay = document.getElementById("play");
+var buttonReset = document.getElementById("reset");
+var tier1 = document.getElementById("Tier1");
+var tier2 = document.getElementById("Tier2");
+var tier3 = document.getElementById("Tier3");
+var h2 = document.getElementsByClassName("h2");
 
 // var pic1 = document.getElementById('pic1');
 // var pic2 = document.getElementById('pic2');
@@ -16,111 +16,93 @@ var Img1Count;
 var Img2Count;
 var LastImageFlipped;
 var CurentImageFlipped;
-
-//PLAY AND RESET BUTTON 
-buttonPlay.addEventListener('click', showCards);
-buttonReset.addEventListener('click', hideCards);
-
+var completedGame;
+var cardCounter = 0;
+//PLAY AND RESET BUTTON
+buttonPlay.addEventListener("click", showCards);
+buttonReset.addEventListener("click", hideCards);
 
 // DISPLAY CARDS AFTER SHOSSING LEVEL
 
-function showCards(e){
-        e.preventDefault();
-  
-  if (document.getElementById('select-game').value === '1') {
+function showCards(e) {
+  e.preventDefault();
+
+  if (document.getElementById("select-game").value === "1") {
     tier1.style.visibility = "visible";
-        }
+  }
 
-    if (document.getElementById('select-game').value === '2') {
-      tier1.style.visibility = "visible";
-      tier2.style.visibility = "visible";
-          }
-  
-        if (document.getElementById('select-game').value === '3') {
-          tier1.style.visibility = "visible";
-          tier2.style.visibility = "visible";
-          tier3.style.visibility = "visible";
-             }
+  if (document.getElementById("select-game").value === "2") {
+    tier1.style.visibility = "visible";
+    tier2.style.visibility = "visible";
+  }
 
-document.getElementById('select-game').value = "0";
-
+  if (document.getElementById("select-game").value === "3") {
+    tier1.style.visibility = "visible";
+    tier2.style.visibility = "visible";
+    tier3.style.visibility = "visible";
+  }
+  completedGame = document.getElementById("select-game").value * 6;
+  document.getElementById("select-game").value = "0";
 }
+
 
 // RESET GAME
-function hideCards(e){
-   e.preventDefault();
+function hideCards(e) {
+  e.preventDefault();
 
    tier1.style.visibility = "hidden";
-  //  tier2.style.visibility = "hidden";
-  //  tier3.style.visibility = "hidden";
+   tier2.style.visibility = "hidden";
+   tier3.style.visibility = "hidden";
 
+  var images = document.getElementsByClassName("images");
+  
+  // images.forEach(element => images.src = "img/Init.jpg");
+  
+  images.src = "img/Init.jpg"; 
+  console.log(images);
+  document.getElementById("select-game").value = "0";
 }
 
-
-
-function CompareImages(img1, img2, SetThisOne, Current) {
-   if (img1 =! img2) {
-     CurentImageFlipped = SetThisOne;
-     Current = SetThisOne;
-
-   } 
-
-   
-   console.log(img1);
-   console.log(img2);
-}
-
-
+//SHOW IMAGE AND COMPARE WITH SECOND IMAGE
 function toggleImage(ImgName, NewImg) {
+  var resetImage = "img/Init.jpg";
+  var imgElement = document.getElementById(ImgName);
+  imgElement.src = NewImg;
+
+  if (Img1Count) {
+    Img2Count = document.getElementById(ImgName).src;
+    CurentImageFlipped = document.getElementById(ImgName);
+    if (Img2Count != Img1Count) {
+      CurentImageFlipped.src = resetImage;
+      LastImageFlipped.src = resetImage;
+      Img1Count = null;
+      Img1Count = null;
+    }
+    if (Img2Count == Img1Count) {
+      CurentImageFlipped.src = "img/OK.jpg";
+      LastImageFlipped.src = "img/OK.jpg";
+      Img1Count = null;
+      Img1Count = null;
+      cardCounter = cardCounter + 2;
+     console.log(cardCounter);
+     console.log(completedGame);
+
+     if (cardCounter == completedGame) {
+
+      tier1.style.visibility = "hidden";
+     tier2.style.visibility = "hidden";
+     tier3.style.visibility = "hidden";
+     window.alert("Congrats you Won!!!")    
+    }
+
+    }
+  } else {
+    Img1Count = document.getElementById(ImgName).src;
+    LastImageFlipped = document.getElementById(ImgName);
+  }
    
-  var img11 = "img/Init.jpg";
-   var imgElement = document.getElementById(ImgName);
-   imgElement.src =  NewImg;
 
-   CurentImageFlipped = imgElement;
-
-
-   CompareImages(LastImageFlipped, CurentImageFlipped, img11, imgElement);
-
-   LastImageFlipped = NewImg;
-
-console.log(imgElement);
-console.log(CurentImageFlipped);
-console.log(Img1Count);
-console.log(Img2Count);
-console.log(LastImageFlipped);
-console.log();
-
-CurentImageFlipped = "";
-
+   
+  
 }
-
-
-
-
-
-
-
-
-// function ShowClickedImage(ImgName, Source){
-
-//   image = document.getElementById(ImgName);
-//   image.src = Source;
-
-// }
-
-
-
-
-// imgClicked2.addEventListener('click', imageClicked2);
-
-//   function imageClicked2()
-//   {
-//     console.log("clicked");
-//     document.getElementById('pic2').visibility = "visible";
-// }
-
-
-
-
 
